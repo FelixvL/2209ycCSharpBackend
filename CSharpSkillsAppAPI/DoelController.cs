32970a06d53243d979efbc79888e5de73a5d8e7e
FelixvL <digitaleservice@gmail.com>
2022-09-20 15:47:12 +0200

    eerste C#

---
+++ CSharpSkillsAppAPI/DoelController.cs
@@ -0,0 +1,63 @@
+﻿using DBContextSkillsDB;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CSharpSkillsAppAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoelController : ControllerBase
+    {
+        private SkillDBContext _db;
+
+
+        public DoelController(SkillDBContext db) { 
+            _db = db;
+            }
+        
+        
+        // GET: api/<DoelController>
+
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            Doel d = new Doel();
+            d.Naam = "Frits";
+            _db.doelen.Add(d);
+            _db.SaveChanges();
+            return new string[] { "DIT GA IK ZIEN", "value2" };
+        }
+                [HttpGet("tweede")]
+        public IEnumerable<Doel> GetTweede()
+        {
+            return _db.doelen;
+        }
+        // GET api/<DoelController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DoelController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DoelController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DoelController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DBContextSkillsDB/Doel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBContextSkillsDB
+{
+    public class Doel
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public int Belangrijkheid { get; set; }
+    }
+}
